738db3c44ee3167c2cc6b958cd45f88d532b10e0
rohitbarche0002 <rohitbarche99@gmail.com>
2023-06-28 09:25:34 +0530

    add structure for redux toolkit and custom hooks and contexts

--- uber-dashboard-BE/Db/connection.js
+++ uber-dashboard-BE/Db/connection.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv .config();
-console.log(process.env.MONGODB_URL,"----")
 
 mongoose.connect(process.env.MONGODB_URL,{ 
 }).then(() => {
